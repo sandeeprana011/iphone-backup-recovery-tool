b2c5f6b9a9eb8575d85e298562906af7f98031f1
Sandeep Rana <sandeep.rana@investo2o.com>
2022-01-14 00:46:39 +0530

    Initial Commit

---
+++ Recover Media From Backup/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Recover Media From Backup
+//
+//  Created by Sandeep Rana on 14/01/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Recover Media From Backup/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Recover Media From Backup
+//
+//  Created by Sandeep Rana on 14/01/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
